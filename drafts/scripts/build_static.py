85f4e0a42872c7a9e49f8f43aab8fac907e94555
fhardison <fhardison@gmail.com>
2019-10-31 09:42:15 -0400

    building build static system

--- drafts/scripts/build_static.py
+++ drafts/scripts/build_static.py
@@ -6,41 +6,111 @@ import re
 import mistletoe
 
 
-
-def make_margin_note(id, content, css_class='', marker=''):
-    return f"""<label for="ln-{id}" class="margin-toggle {css_class}">{marker}</label>
-<input type="checkbox" id="ln-{id}" class="margin-toggle"/>
-<span class="marginnote">{content}</span>"""
-
-def make_line_num(num):
-    return make_margin_note(num, f"<i>{num}</i>")
-
-def make_sidenote(line, fn_content, fn_num):
-    return line.replace(f" [{fn_num}]",
-        make_margin_note(fn_num, fn_content, css_class='sidenote-number'), 1)
-        # only make one replacement in case of multiple fns with same fn_num
-        #  which should not happen, but who knows
+class Renderer():
+    def render(self, ref, cons):
+        pass
 
 
+class StaticRenderer():
+    def __init__(self):
+        self.renderers = {}
+        self.buffers = {}
+        self.patterns = []
+        self.line_renderers = []
 
-def convert_line_numbers(line, line_count):
-    line_nums = re.findall(r'\{(\d*\.?\d+)\}', line)
-    print(line_nums)
-    if not(line_nums):
-        return (line, line_count)
-    for line_num in line_nums:
-        #handle line numbers
-        if '.' in line_num:
-            line_count += 5
-            line = line.replace("{" + str(line_num)+ "}", make_line_num(line_count))
-        #remove page refs
+    def add_renderer(self, pattern, f):
+        self.buffers[pattern] = []
+        self.renderers[pattern] = f
+        self.patterns.append(pattern)
+
+    def add_line_renderer(self, f):
+        self.line_renderers.append(f)
+
+    def add_if_buffer(self, ref):
+        for pattern in self.patterns:
+            if pattern in ref:
+                self.buffers[pattern].append(ref)
+
+    def render_buffers(self, cons):
+        for key, buffer in self.buffers.items():
+            renderer = self.renderers[key]
+            for ref in buffer:
+                renderer.render(ref, cons)
+
+    def render_lines(self, cons, printf):
+        for line in cons:
+            for renderer in self.line_renderers:
+                line = renderer.render("", line)
+            printf(line)
+
+class LineNumberRenderer(Renderer):
+    def __init__(self):
+        self.line_counter = 0
+
+    def make_margin_note(self, id, content, css_class='', marker=''):
+        return f"""<label for="ln-{id}" class="margin-toggle {css_class}">{marker}</label>
+                    <input type="checkbox" id="ln-{id}" class="margin-toggle"/>
+                    <span class="marginnote">{content}</span>"""
+
+    def make_line_num(self, num):
+        return self.make_margin_note(num, f"<i>{num}</i>")
+
+
+    def render(self, ref, line):
+        line_nums = re.findall(r'\{(\d*\.?\d+)\}', line)
+        print(line_nums)
+        if not(line_nums):
+            return line
+        for line_num in line_nums:
+            #handle line numbers
+            if '.' in line_num:
+                self.line_counter += 5
+                line = line.replace("{" + str(line_num)+ "}", self.make_line_num(self.line_counter))
+            #remove page refs
+            else:
+                line = line.replace("{" + str(line_num)+ "}",'')
+
+        return line.replace('  ', ' ') #clean up any double spaces left by line/page num markers
+
+class HeaderRenderer(Renderer):
+    def render(self, ref, cons):
+        if "#" in cons:
+            header_lvl = cons.count('#')
+            return f"<h{header_lvl}>{cons.replace('#', '').strip()}</h{header_lvl}>"
         else:
-            line = line.replace("{" + str(line_num)+ "}",'')
+            return cons
 
-    return (line.replace('  ', ' '), line_count) #clean up any double spaces left by line/page num markers
+class SideNoteRenderer(Renderer):
+    def make_margin_note(self, id, content, css_class='', marker=''):
+        return f"""<label for="ln-{id}" class="margin-toggle {css_class}">{marker}</label>
+    <input type="checkbox" id="ln-{id}" class="margin-toggle"/>
+    <span class="marginnote">{content}</span>"""
+
+    def make_line_num(self, num):
+        return self.make_margin_note(num, f"<i>{num}</i>")
+
+    def make_sidenote(self, line, fn_content, fn_num):
+        return line.replace(f" [{fn_num}]",
+            self.make_margin_note(fn_num, fn_content, css_class='sidenote-number'), 1)
+            # only make one replacement in case of multiple fns with same fn_num
+            #  which should not happen, but who knows
+
+    def render(self, ref, cons):
+        ref_parts = ref.split('.')
+        fn_num = ref_parts[-1]
+        ref_num = '.'.join(ref_parts[:-1])
+        cons[ref_num] = self.make_sidenote(cons[ref_num], cons[ref], fn_num)
+        del cons[ref] #remove FN after processing
+
+renderer = StaticRenderer()
+
+renderer.add_renderer("fn", SideNoteRenderer())
+renderer.add_line_renderer(LineNumberRenderer())
+renderer.add_line_renderer(HeaderRenderer())
+
 
 def make_title(raw):
-    return raw.replace('greek-boy-ch', 'Chapter ').replace('.md', '').replace("..\\..\\src\\", '')
+    return raw.replace('greek-boy-ch', 'Chapter ').replace('.md', '').replace(os.path.join("..", "..", "src") + "/", '')
 
 
 WORK_LIST = glob.glob(os.path.join('..','..', 'src', '*.md'))
@@ -50,7 +120,8 @@ for WORK in WORK_LIST:
     SRC = WORK
     DEST = WORK.replace("src", "docs").replace(".md", ".html")
     TITLE = 'A Greek Boy at Home, ' + make_title(SRC)
-    HEADER = f"""\
+    print(TITLE)
+    HEADER = f"""
     <!DOCTYPE html>
     <html lang="grc">
     <head>
@@ -85,26 +156,12 @@ for WORK in WORK_LIST:
                 ref = parts[0]
                 content = parts[1]
                 if not(content in cons):
-                    if "fn" in ref:
-                        fn_buffer.append(ref)
+                    renderer.add_if_buffer(ref)
                     cons[ref] = content
                 else:
                     print("Multiple lines with same ref number in file!")
-            for fn in fn_buffer:
-                ref_parts = fn.split('.')
-                fn_num = ref_parts[-1]
-                ref_num = '.'.join(ref_parts[:-1])
-                cons[ref_num] = make_sidenote(cons[ref_num], cons[fn], fn_num)
-                del cons[fn] #remove FN after processing
-            for ref, content in cons.items():
-                #handle headers
-                if "#" in content:
-                    header_lvl = content.count('#')
-                    print(f"<h{header_lvl}>{content.replace('#', '').strip()}</h{header_lvl}>", file=g)
-                #handle content
-                else:
-                    #convert line numbers
-                    content, line_counter = convert_line_numbers(content, line_counter)
-                    print(f"{mistletoe.markdown(content)}", file=g)
+            renderer.render_buffers(cons)
+            print(len(cons))
+            renderer.render_lines(list(cons.values()), lambda x: print(f"{mistletoe.markdown(x)}", file=g))
             print(FOOTER, file=g)
 print("Done!")
