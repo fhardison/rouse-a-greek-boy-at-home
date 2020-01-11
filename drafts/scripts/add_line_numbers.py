1b1facdd8de24e5a83e4dd7ab0ad8d729dab5687
fhardison <fhardison@gmail.com>
2020-01-11 14:53:41 -0500

    13 finished

--- drafts/scripts/add_line_numbers.py
+++ drafts/scripts/add_line_numbers.py
@@ -1,8 +1,14 @@
-import sys
+import sys, re, os
 
 ifile = sys.argv[1]
-ofile = sys.argv[2]
-cpt_num = f"{int(sys.argv[3]):2d}"
+#ofile = sys.argv[2]
+cpt_num = re.search("greek-boy-ch(\d+).md", ifile)[1]
+
+ofile = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'src', ifile)
+
+
+#cpt_num = f"{int(sys.argv[3]):2d}"
+
 
 line_num = 0
 with open(ifile, 'r', encoding="UTF-8") as i:
